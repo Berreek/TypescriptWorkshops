20da9b1ba42b1dca8e69fb8bf6d99372729358af
berrek <bartoszbehring@gmail.com>
2018-04-11 23:02:34 +0200

    Add tax controller

---
+++ TypescriptWorkshops/TypescriptWorkshops/src/modules/testApplicationModule/controllers/tax.controller.ts
@@ -0,0 +1,11 @@
+﻿import { Controller, Get, Query } from "@nestjs/common";
+import {ForeignTransferParams} from "../models/foreignTransferParams";
+
+@Controller("tax")
+export class TaxController {
+
+    @Get()
+    get( @Query() queryParams: ForeignTransferParams) {
+        return null;
+    }
+}
